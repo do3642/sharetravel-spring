2f785e9a43ed13472de82d37aeaa01ac6cd0790f
yc <do36421@naver.com>
2024-11-20 12:39:11 +0900

    Add: User회원가입 기능 구현

--- src/main/java/com/mbc/sharetravel_spring/controller/UserController.java
+++ src/main/java/com/mbc/sharetravel_spring/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.mbc.sharetravel_spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mbc.sharetravel_spring.domain.Member;
 import com.mbc.sharetravel_spring.domain.TestVO;
 import com.mbc.sharetravel_spring.service.MemberService;
-
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -20,7 +22,12 @@ public class UserController {
 	private MemberService memberService;
 	
 	
-
+	@PostMapping("/register")
+	public ResponseEntity<?> signup(@RequestBody Member member) {
+		memberService.insert(member);
+		
+		return new ResponseEntity<>("회원가입성공",HttpStatus.OK);
+	}
 
 	
 
